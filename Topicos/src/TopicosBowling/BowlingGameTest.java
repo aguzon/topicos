c2d00b6b3bc391f6c22a9b4b4415404922d03352
Zoni Agustin <agustinzoni@hotmail.com>
2017-11-27 18:31:11 -0300

    comint rojo

--- Topicos/src/TopicosBowling/BowlingGameTest.java
+++ Topicos/src/TopicosBowling/BowlingGameTest.java
@@ -8,17 +8,50 @@ import org.junit.*;
 
 public class BowlingGameTest {
 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
+	private BowlingGame g;
+
+	void rollMany(int n, int pins, BowlingGame g) {
+		for (int i = 0; i < n; i++) g.roll(pins);
 	}
-	@Before 
-	public void before{
-		
+
+	@Before
+	public void setUp() {
+		this.g = new BowlingGame();
+	}
+
+	@Test
+	public void testZero() {
+		rollMany(20, 0, g);
+		assertEquals(0, g.score());
 	}
 	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testAllOnes() {
+		rollMany(20, 1, g);
+		assertEquals(20, g.score());
 	}
 
+	@Test
+	public void testOneSpare() {
+		g.roll(5);
+		g.roll(5);
+		g.roll(3);
+		rollMany(17, 0, g);
+		assertEquals(16, g.score());
+	}
+	
+	@Test
+	public void testOneStrike() {
+		g.roll(10);
+		g.roll(3);
+		g.roll(4);
+		rollMany(16, 0, g);
+		assertEquals(24, g.score());
+	}
+	
+	@Test
+	public void testPerfectGame() {
+		rollMany(12, 10, g);
+		assertEquals(300, g.score());
+	}
 }
