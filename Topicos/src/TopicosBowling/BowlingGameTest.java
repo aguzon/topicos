da66587b6fec2e8a3b18eaab6720e8253d0b772e
Zoni Agustin <agustinzoni@hotmail.com>
2017-11-27 19:10:05 -0300

    comminte

--- Topicos/src/TopicosBowling/BowlingGameTest.java
+++ Topicos/src/TopicosBowling/BowlingGameTest.java
@@ -31,28 +31,31 @@ public class BowlingGameTest {
 		assertEquals(10, g.score());
 	}
 
-//	@Test
-//	public void testOneSpare() {
-//		g.roll(5);
-//		g.roll(5);
-//		g.roll(3);
-//		rollMany(17, 0, g);
-//		assertEquals(16, g.score());
-//	
-//	}
-//	
-//	@Test
-//	public void testOneStrike() {
-//		g.roll(10);
-//		g.roll(3);
-//		g.roll(4);
-//		rollMany(16, 0, g);
-//		assertEquals(24, g.score());
-//	}
+	@Test
+	public void testOneSpare() {
+		g.roll(5);
+		g.roll(5);
+		g.roll(3);
+		rollMany(17, 0, g);
+		assertEquals(16, g.score());
 	
-//	@Test
-//	public void testPerfectGame() {
-//		rollMany(12, 10, g);
-//		assertEquals(300, g.score());
-//	}
+	}
+//	para este caso tira 10 luego se cuenta dobel
+	@Test
+	public void testOneStrike() {
+		g.roll(10);
+		g.roll(3);
+		g.roll(4);
+		rollMany(16, 0, g);
+		assertEquals(24, g.score());
+	}
+	
+	@Test
+	public void testPerfectGame() {
+		rollMany(12, 10, g);
+		assertEquals(300, g.score());
+	}
+	para este test 
+	@Test 
+	public void test
 }
